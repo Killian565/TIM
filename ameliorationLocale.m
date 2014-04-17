bf3e6a98729d8e34e6ccb4886a2c50fd23af56f3
FesseNoz <jean.melou@gmail.com>
2014-04-17 10:32:55 +0200

    Create ameliorationLocale.m

---
+++ ameliorationLocale.m
@@ -0,0 +1,23 @@
+function meilleurSigma = ameliorationLocale(image,sigma,P)
+
+  voisinageEchange = voisinage_echange(sigma);
+  voisinageInsertion = voisinage_insertion(sigma);
+
+  meilleurSigma = sigma;
+  meilleureCorr = calculCorrelation(sigma,image,P);
+
+  for i=1:size(voisinageEchange,1)
+    if (calculCorrelation(voisinageEchange(i,:),image,P)>meilleureCorr)
+      meilleurSigma = voisinageEchange(i,:);
+      meilleureCorr = calculCorrelation(voisinageEchange(i,:),image,P);
+    end
+  end
+
+  for i=1:size(voisinageInsertion,1)
+    if (calculCorrelation(voisinageInsertion(i,:),image,P)>meilleureCorr)
+      meilleurSigma = voisinageInsertion(i,:);
+      meilleureCorr = calculCorrelation(voisinageInsertion(i,:),image,P);
+    end
+  end
+  
+end
