9889d891bf9c495c96d9aae4ae0ff32083f3559a
FesseNoz <jean.melou@gmail.com>
2014-04-17 10:39:42 +0200

    Update correlation.m

--- correlation.m
+++ correlation.m
@@ -1,16 +1,6 @@
-function resultat = correlation(vect1,vect2,sens)
+function resultat = correlation(vect1,vect2)
 
-if (strcmp(sens,'horizontal'))
-  vect1 = vect1';
-  vect2 = vect2';
+M = cov(vect1,vect2);
+resultat = M(1,2)/sqrt(M(1,1)*M(2,2));
+
 end
-
-moy1 = mean(vect1);
-moy2 = mean(vect2);
-
-var1 = mean(vect1.^2) - moy1.*moy1;
-var2 = mean(vect2.^2) - moy2.*moy2;
-
-covariance = mean(vect1.*vect2) - moy1.*moy2;
-
-resultat = covariance/sqrt(var1*var2);
