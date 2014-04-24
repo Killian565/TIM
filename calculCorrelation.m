118ea9ca3350630e9306095c0a3abab17874dcc9
Killian565 <kfreyermuth.n7@gmail.com>
2014-04-24 16:01:51 +0200

    Update calculCorrelation.m

--- calculCorrelation.m
+++ calculCorrelation.m
@@ -6,31 +6,43 @@ sommeCor = 0;
 
 for k=1:size(sigma,2)
 
-  if mod(k,Q)~=0
-    posK = rg2pos(sigma(k),P,Q)+1;
-    posKdroite = rg2pos(sigma(k+1),P,Q)+1;
-
-    for i=1:P
-      vect1(i) = im(posK(1)+i-1,posK(2)+P);
-      vect2(i) = im(posKdroite(1)+i-1,posKdroite(2));
+    for i = 1:size(sigma,2)
+        aux = sigma(i);
+        sigma_moins_1(aux) = i;
     end
-    sommeCor = sommeCor +  correlation(vect1,vect2);
+  
+    if mod(k,Q)~=0
+        posK = rg2pos(sigma_moins_1(k),P,Q)+1;
+        posKdroite = rg2pos(sigma_moins_1(k+1),P,Q)+1;
 
-  end
+        vect1 = im(posK(1):posK(1)+P-1,posK(2)+P-1);
+        vect2 = im(posKdroite(1):posKdroite(1)+P-1,posKdroite(2)+P-1);
 
-  if (Q^2-Q>k)
-    posK = rg2pos(sigma(k),P,Q)+1;
-    posKdessous = rg2pos(sigma(k+Q),P,Q)+1;
+        if max(vect1)~=0 && max(vect2)~=0
+            sommeCor = sommeCor +  correlation(vect1,vect2);
+        else
+            %sommeCor = sommeCor-0.5;
+        end
 
-    for i=1:P
-      vect1(i) = im(posK(1)+P-1,posK(2)+i-1);
-      vect2(i) = im(posKdessous(1),posKdessous(2)+i-1);
     end
-    sommeCor = sommeCor +  correlation(vect1,vect2);
 
-  end
+    if (Q^2-Q>k)
+        posK = rg2pos(sigma_moins_1(k),P,Q)+1;
+        posKdessous = rg2pos(sigma_moins_1(k+Q),P,Q)+1;
+
+        vect1 = im(posK(1)+P-1,posK(2):posK(2)+P-1);
+        vect2 = im(posKdessous(1),posKdessous(2):posKdessous(2)+P-1);
+
+        if max(vect1)~=0 && max(vect2)~=0
+            sommeCor = sommeCor +  correlation(vect1,vect2);
+        else
+            % sommeCor =  sommeCor-0.5;
+        end
+    
+    end
 
 end
 
 
 resultat = sommeCor;
+
