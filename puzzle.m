4afd6533287b318aa718ae3fc95b342803c80334
Killian565 <kfreyermuth.n7@gmail.com>
2014-04-24 16:04:24 +0200

    Update puzzle.m

--- puzzle.m
+++ puzzle.m
@@ -1,7 +1,10 @@
 %Script de résolution de Puzzle:
-
+close all;
 lectri;
 sigma = randperm(16);
 P = 128;
-ameliorationLocale(im,sigma,P);
+%sigma = recuit_simule(im,sigma,P);
+sigma = ameliorationLocale(im,sigma,P);
+sigma = recuit_simule(im,sigma,P);
+
 IM = permutationImage(im, sigma, P);
